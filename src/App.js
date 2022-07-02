3c4710db20f76d21bcb248169381fcaa8b29c424
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-07-02 20:23:58 +0200

    Project boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <>
+    <div className="container pt-5">
       <h1>Hello React</h1>
-    </>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './public/assets/css/global.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
